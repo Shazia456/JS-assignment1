923f73cefdbec46da0221d7930112ad256f86d38
Shazia456 <128918032+Shazia456@users.noreply.github.com>
2023-04-05 12:35:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let firstName="Hello world";
+
+alert(firstName);
+
+alert("Error please enter a valid password");
+
+alert("Welcome to JS Land...\n Happy coding");
+
+alert("Welcome to JS Land");
+
+alert("Happy coding");
+
+alert("Hello...I can run JS through my web browsers console ");
